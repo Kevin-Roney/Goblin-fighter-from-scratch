95cf6fffd690d0e0f872df984e314b7708e51b09
Kevin Roney <ktroney.pdx@gmail.com>
2022-03-17 11:09:31 -0700

    grabbed DOM and set state in apps.js

--- app.js
+++ app.js
@@ -1,7 +1,25 @@
 // import functions and grab DOM elements
-
+const goblinKillCount = document.getElementById('goblin-counter');
+const playerHealthBar = document.getElementById('health-bar');
+const playerSprite = document.getElementById('sprite');
+const goblinNameInput = document.getElementById('goblin-input');
+const goblinAddButton = document.getElementById('add-goblin-button');
 // let state
-
+let playerHP = 10;
+const goblins = [
+    {
+        name: 'Jimothy',
+        goblinHP: 4
+    },
+    {
+        name: 'Doctor Garbage',
+        goblinHP: 2
+    },
+    {
+        name: 'Maestro Fabreezio',
+        goblinHP: 3
+    }
+];
 // set event listeners 
   // get user input
   // use user input to update state 
