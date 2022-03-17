65d819a6b5873d43612229783223958925da5aa2
Kevin Roney <ktroney.pdx@gmail.com>
2022-03-17 13:52:40 -0700

    moved goblinListEl eventListener inside of display goblins

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // import functions and grab DOM elements
 import { renderGoblin } from './render-utils.js';
 
-const goblinsEl = document.getElementById('goblin-list');
+const goblinListEl = document.getElementById('goblin-list');
 const goblinKillCount = document.getElementById('goblin-counter');
 const playerHealthBar = document.getElementById('health-bar');
 const playerSprite = document.getElementById('sprite');
@@ -38,36 +38,38 @@ goblinAddButton.addEventListener('click', () => {
     goblinNameInput.value = '';
     displayGoblins();
 });
-goblinsEl.addEventListener('click', () => {
-  //player hit
-    if (Math.random() > .5) {
-        goblins.goblinHP--;
-        playerSprite.src = '#############';
-        alert('you hit ' + goblins.name + '!!');
-    } else {
-        alert('Oh noes you missed OwO!!');
-    }
-  //goblin hits
-    if (Math.random() < .33) {
-        playerHP--;
-        playerHealthBar.textContent = `You have ${playerHP} left!!`;
-        alert(goblins.name + 'hit you!');
-    } else {
-        alert(goblins.name + 'missed!!');
-    }
-    //goblins defeated
-    if (goblins.goblinHP === 0) {
-        goblinsDefeated++;
-        goblinKillCount.textContent = `You have defeated ${goblinsDefeated} goblins!`;
-    }
-});
+
 function displayGoblins() {
-    goblinsEl.textContent = '';
-    const goblinEl = renderGoblin(goblins.name, goblins.goblinHP);
+    goblinListEl.textContent = '';
     for (let goblin of goblins) {
-        renderGoblin(goblin, goblins);
-        displayGoblins();
+        const goblinEl = renderGoblin(goblins);
+        if (goblin.goblinHP > 0) {
+            goblinEl.addEventListener('click', () => {
+      //player hit
+                if (Math.random() > .5) {
+                    goblins.goblinHP--;
+                    playerSprite.src = '#############';
+                    alert('you hit ' + goblins.name + '!!');
+                } else {
+                    alert('Oh noes you missed OwO!!');
+                }
+                  //goblin hits
+                if (Math.random() < .33) {
+                    playerHP--;
+                    playerHealthBar.textContent = `You have ${playerHP} left!!`;
+                    alert(goblins.name + 'hit you!');
+                } else {
+                    alert(goblins.name + 'missed!!');
+                }
+                    //goblins defeated
+                if (goblins.goblinHP === 0) {
+                    goblinsDefeated++;
+                    goblinKillCount.textContent = `You have defeated ${goblinsDefeated} goblins!`;
+                }
+            });
+        }
+    
+        goblinListEl.append(goblinEl);
     }
-    goblinsEl.append(goblinEl);
 }
 displayGoblins();
