e0d21a7867e9aef5ae413c5475f4506d069a5620
Kevin Roney <ktroney.pdx@gmail.com>
2022-03-17 11:54:09 -0700

    added goblinsEl eventListener

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 // import functions and grab DOM elements
+const goblinsEl = document.getElementById('goblin-list');
 const goblinKillCount = document.getElementById('goblin-counter');
 const playerHealthBar = document.getElementById('health-bar');
 const playerSprite = document.getElementById('sprite');
@@ -6,6 +7,7 @@ const goblinNameInput = document.getElementById('goblin-input');
 const goblinAddButton = document.getElementById('add-goblin-button');
 // let state
 let playerHP = 10;
+let goblinsDefeated = 0;
 const goblins = [
     {
         name: 'Jimothy',
@@ -31,6 +33,27 @@ goblinAddButton.addEventListener('click', () => {
         goblinHP: Math.ceil(Math.random() * 5)
     };
     goblins.push(newGoblin);
+    goblinNameInput.value = '';
     displayGoblins();
-    
+});
+goblinsEl.addEventListener('click', () => {
+  //player hit
+    if (Math.random() > .5) {
+        goblins.goblinHP--;
+        alert('you hit ' + goblins.name + '!!');
+    } else {
+        alert('Oh noes you missed OwO!!');
+    }
+  //goblin hits
+    if (Math.random() < .33) {
+        playerHP--;
+        alert(goblins.name + 'hit you!');
+    } else {
+        alert(goblins.name + 'missed!!');
+    }
+    //goblins defeated
+    if (goblins.goblinHP === 0) {
+        goblinsDefeated++;
+        goblinKillCount.textContent = `You have defeated ${goblinsDefeated} goblins!`;
+    }
 });
